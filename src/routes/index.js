64f9a08719425488181ffe2b4f4ee82e03d18cfc
ericnshimiye <ericnsh@gmail.com>
2019-11-02 23:49:27 -0400

    Add user signup

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const users = require('./users');
 const httpStatus = require('http-status-codes');
 
 // @route   GET /health
 // @desc    Health check endpoint
 // @access  public
 router.get('/health', (_req, res) => res.status(httpStatus.OK).send());
+
+// @route   POST /api/users/signup
+// @desc    Register a user
+// @access  public
+router.post('/api/users/signup', users.signup);
 
 module.exports = router;
---
+++ src/routes/users.js
@@ -0,0 +1,49 @@
+const bcrypt = require("bcryptjs");
+const httpStatus = require('http-status-codes');
+const validateSignUpInput = require("../validation/register");
+const userModel = require("../dal/models/users");
+const teamModel = require("../dal/models/teams");
+
+exports.signup = (req, res) => {
+    const { errors, isValid } = validateSignUpInput(req.body);
+    
+    if (!isValid) {
+        return res.status(httpStatus.BAD_REQUEST).json(errors);
+    }
+    
+    userModel.findOne({ email: req.body.email })
+        .then(user => {
+            if (user) {
+                errors.email = "User with this email already exists";
+                return res.status(httpStatus.BAD_REQUEST).json(errors);
+            } else {
+                teamModel.findOne({ code: req.body.team })
+                    .then(team => {
+                        if(!team){
+                            errors.team = "Team not found";
+                            return res.status(httpStatus.BAD_REQUEST).json(errors);
+                        }
+
+                        const newUser = new userModel({
+                            firstName: req.body.firstName,
+                            lastName: req.body.lastName,
+                            email: req.body.email,
+                            password: req.body.password,
+                            team: team.id
+                        });
+
+                        bcrypt.genSalt(10, (err, salt) => {
+                            bcrypt.hash(newUser.password, salt, (err, hash) => {
+                                if (err) throw err;
+                                newUser.password = hash;
+                                newUser
+                                    .save()
+                                    .then(user => res.json(user))
+                                    .catch(err => console.log(err));
+                            });
+                        });
+                    });
+            }
+        })
+        .catch(err => console.log(err));
+};
