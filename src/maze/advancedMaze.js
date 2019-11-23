41831c8b765a764fd81778b6a887378ecea6743d
ericnshimiye <ericnsh@gmail.com>
2019-11-23 12:13:52 -0500

    Adjust maze definition

--- src/maze/advancedMaze.js
+++ src/maze/advancedMaze.js
@@ -437,7 +437,7 @@ const mazeDefinition = [
         {top: 1, right: 1, left: 0, bottom: 0}, // 17
         {top: 1, right: 0, left: 1, bottom: 0}, // 18
         {top: 1, right: 1, left: 0, bottom: 0}, // 19
-        {top: 0, right: 0, left: 1, bottom: 1} // 20
+        {top: 0, right: 1, left: 1, bottom: 0} // 20
     ]
 ];
 
