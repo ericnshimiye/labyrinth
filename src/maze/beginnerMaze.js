fe6bc88ce4225769c0c60a419951921d8cd0d108
ericnshimiye <ericnsh@gmail.com>
2019-11-23 10:41:56 -0500

    Adjust labyrinth definition

--- src/maze/beginnerMaze.js
+++ src/maze/beginnerMaze.js
@@ -1,37 +1,37 @@
 const mazeDefinition = [
     [
         {top: 1, right: 1, left: 0, bottom: 1},
-        {top: 0, right: 1, left: 0, bottom: 0},
+        {top: 0, right: 1, left: 1, bottom: 0},
         {top: 0, right: 0, left: 0, bottom: 1},
         {top: 0, right: 1, left: 0, bottom: 1},
-        {top: 0, right: 0, left: 0, bottom: 1}
+        {top: 0, right: 0, left: 1, bottom: 1}
     ],
     [
         {top: 1, right: 0, left: 0, bottom: 0},
         {top: 0, right: 1, left: 0, bottom: 1},
-        {top: 1, right: 0, left: 0, bottom: 1},
+        {top: 1, right: 0, left: 1, bottom: 1},
         {top: 1, right: 0, left: 0, bottom: 0},
         {top: 1, right: 0, left: 0, bottom: 1}
     ],
     [
         {top: 0, right: 1, left: 0, bottom: 1},
-        {top: 1, right: 0, left: 0, bottom: 0},
+        {top: 1, right: 0, left: 1, bottom: 0},
         {top: 1, right: 1, left: 0, bottom: 0},
-        {top: 0, right: 1, left: 0, bottom: 0},
-        {top: 1, right: 0, left: 0, bottom: 1}
+        {top: 0, right: 1, left: 1, bottom: 0},
+        {top: 1, right: 0, left: 1, bottom: 1}
     ],
     [
         {top: 1, right: 0, left: 0, bottom: 1},
         {top: 0, right: 1, left: 0, bottom: 1},
-        {top: 0, right: 1, left: 0, bottom: 0},
-        {top: 0, right: 0, left: 0, bottom: 1},
+        {top: 0, right: 1, left: 1, bottom: 0},
+        {top: 0, right: 0, left: 1, bottom: 1},
         {top: 1, right: 0, left: 0, bottom: 1}
     ],
     [
         {top: 1, right: 1, left: 0, bottom: 0},
-        {top: 1, right: 0, left: 0, bottom: 0},
+        {top: 1, right: 0, left: 1, bottom: 0},
         {top: 0, right: 1, left: 0, bottom: 0},
-        {top: 1, right: 0, left: 0, bottom: 0},
+        {top: 1, right: 0, left: 1, bottom: 0},
         {top: 1, right: 1, left: 0, bottom: 0}
     ]
 ];
